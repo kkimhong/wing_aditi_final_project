4c60de5d10c10ce415a17f4c86f8f0d751d1f3cb
k.hong <kruykimhongkkh@gmail.com>
2026-04-03 21:12:16 +0700

    revert back to original

--- src/main/java/com/kkimhong/expensetracker/configs/SecurityConfig.java
+++ src/main/java/com/kkimhong/expensetracker/configs/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://wingfinalfrontend.vercel.app"));
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
